36b02ec74549d69ee610adf6c346a53212821699
toanong <chitoan@Hiens-MacBook-Air.local>
2014-11-10 13:45:10 -0700

    Remove vocabulary restriction in procedure query

--- PEDSnet_PCORnet_ETL/OMOP-PCORNet r3.3.sql
+++ PEDSnet_PCORnet_ETL/OMOP-PCORNet r3.3.sql
@@ -139,7 +139,7 @@ select distinct
 from
 	omop.procedure_occurrence po
 	join pcornet.encounter enc on cast(po.visit_occurrence_id as text)=enc.encounterid
-	join rz.concept c on po.procedure_concept_id=c.concept_id and vocabulary_id=1;
+	join rz.concept c on po.procedure_concept_id=c.concept_id;
 
 -- observation --> vital WITHOUT Observation time
 insert into pcornet.vital(
