0073206b5ba58cb18b664471f2a9ed29686abe3a
Ritu Khare <kharer@email.chop.edu>
2014-12-16 09:19:27 -0500

    Delete cz_omop_pcornet_concept_map_ddl.sql
    PCORnet DDLs are stored in the [PCORnet folder] (https://github.com/PEDSnet/Data_Models/tree/master/PCORnet)

--- PEDSnet_PCORnet_ETL/ETL Scripts/cz_omop_pcornet_concept_map_ddl.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE cz.cz_omop_pcornet_concept_map
-(
-  target_concept character varying(200),
-  source_concept_class character varying(200),
-  source_concept_id integer,
-  value_as_concept_id integer,
-  concept_description character varying(200)
-)
