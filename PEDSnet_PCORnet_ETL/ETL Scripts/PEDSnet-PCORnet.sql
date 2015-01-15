c6a3ff382c2bac23c8fc7f5d6cf3113502ef3930
toanong <toan.ong@ucdenver.edu>
2015-01-15 14:36:50 -0700

    Fix schema name in the encounter query

--- PEDSnet_PCORnet_ETL/ETL Scripts/PEDSnet-PCORnet.sql
+++ PEDSnet_PCORnet_ETL/ETL Scripts/PEDSnet-PCORnet.sql
@@ -67,17 +67,17 @@ v.care_site_id as facilityid,
 	null as raw_drg_type,
 	case when o4.person_id is null then null else cast(o4.value_as_concept_id as text) end as raw_admitting_source
 from 
-	chop_transform.visit_occurrence v
+	omop.visit_occurrence v
 	left join omop.care_site c on v.care_site_id = c.care_site_id
 	left join omop.location l on c.location_id = l.location_id
 	left join o1 on v.visit_occurrence_id = o1.visit_occurrence_id 
 	left join o2 on v.visit_occurrence_id = o2.visit_occurrence_id 
 	left join o3 on v.visit_occurrence_id = o3.visit_occurrence_id 
 	left join o4 on v.visit_occurrence_id = o4.visit_occurrence_id 
-	left join pcornet.cz_omop_pcornet_concept_map m1 on case when v.place_of_service_concept_id is null AND m1.source_concept_id is null then true else 	v.place_of_service_concept_id = m1.source_concept_id end and m1.source_concept_class='Encounter type'
-	left join pcornet.cz_omop_pcornet_concept_map m2 on case when o1.value_as_concept_id is null AND m2.value_as_concept_id is null then true else o1.value_as_concept_id = m2.value_as_concept_id end and m2.source_concept_class='Discharge disposition'
-	left join pcornet.cz_omop_pcornet_concept_map m3 on case when o3.value_as_concept_id is null AND m3.value_as_concept_id is null then true else o3.value_as_concept_id = m3.value_as_concept_id end and m3.source_concept_class='Discharge status'
-	left join pcornet.cz_omop_pcornet_concept_map m4 on case when o4.value_as_concept_id is null AND m4.value_as_concept_id is null then true else o4.value_as_concept_id = m4.value_as_concept_id end and m4.source_concept_class='Admitting source'
+	left join cz.cz_omop_pcornet_concept_map m1 on case when v.place_of_service_concept_id is null AND m1.source_concept_id is null then true else 	v.place_of_service_concept_id = m1.source_concept_id end and m1.source_concept_class='Encounter type'
+	left join cz.cz_omop_pcornet_concept_map m2 on case when o1.value_as_concept_id is null AND m2.value_as_concept_id is null then true else o1.value_as_concept_id = m2.value_as_concept_id end and m2.source_concept_class='Discharge disposition'
+	left join cz.cz_omop_pcornet_concept_map m3 on case when o3.value_as_concept_id is null AND m3.value_as_concept_id is null then true else o3.value_as_concept_id = m3.value_as_concept_id end and m3.source_concept_class='Discharge status'
+	left join cz.cz_omop_pcornet_concept_map m4 on case when o4.value_as_concept_id is null AND m4.value_as_concept_id is null then true else o4.value_as_concept_id = m4.value_as_concept_id end and m4.source_concept_class='Admitting source'
 
 -- condition_occurrence --> Diagnosis
 insert into pcornet.diagnosis(
